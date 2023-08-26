1aa617af78f1d03fcb117da29df6edfe4a25ed91
Pelumiii1 <adesokanpelumi14@gmail.com>
2023-08-26 13:10:15 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
+import emailjs from "@emailjs/browser";
 
 function App() {
+  const form = useRef();
+
+  // Function to handle form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_h9fm5p7",
+        "template_oidd218",
+        form.current,
+        "EEVwSYA6R0m-_8toc"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          // console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit} ref={form}>
+        <label>Full Name:</label>
+        <input type="text" name="fullName" required />
+        <label>Age:</label>
+        <input type="number" name="age" required />
+        <label>Address:</label>
+        <input type="text" name="address" required />
+        <label>State:</label>
+        <input type="text" name="state" required />
+        <label>City:</label>
+        <input type="text" name="city" required />
+        <label>Zip Code:</label>
+        <input type="number" name="zipCode" required />
+        <label>Phone Number:</label>
+        <input type="number" name="phoneNumber" required />
+        <label>Tracker Number:</label>
+        <input type="number" name="trackerNumber" required />
+        <div className="background__check">
+          <label>Background Check:</label>
+          <input
+            type="checkbox"
+            name="backgroundCheck"
+            className="background__check__input"
+          />
+        </div>
+        <div className="button">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
